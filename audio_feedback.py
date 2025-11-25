75d6126561a03c51653d7ce5404541a154da6f41
adrijrm <141690375+adrijrm@users.noreply.github.com>
2025-11-25 14:49:29 -0600

    audio_feedback.py

---
+++ audio_feedback.py
@@ -0,0 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('rate', 170)
+engine.setProperty('voice', 'spanish')  # o 'english' según tu sistema
+
+def speak(text):
+    print(f"[AUDIO] {text}")
+    engine.say(text)
+    engine.runAndWait()
